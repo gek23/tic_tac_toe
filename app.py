f4e2cce0a8ee9a31d3c0b478c941e110f75e21cc
gek23 <kimeungina56@gmail.com>
2019-08-07 15:27:29 -0700

    main app for the project

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+
+    def draw():
+        game_state = [[None, None, None], [None, None, None], [None, None, None]]
+
+    return render_template("tic_tac.html", game_state=game_state)
+
+
+
+@app.route("/register")
+def register():
+    return "hello"
+
+@app.route("/00")
+def zerozero():
+    return render_template("zerozero.html")
+
